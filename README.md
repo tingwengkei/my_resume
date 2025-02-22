f7a8cd94309d03d030eea1e093bd2f3b621d0a5c
fpletingwengkei <wengkei@setlary.my>
2025-02-22 12:24:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Résumé 
+
+Ting Weng Kei
---
+++ profile.js
@@ -0,0 +1,159 @@
+const profileData = {
+  title: "Resume - TWK",
+  name: "Ting Weng Kei",
+  sub_title: "Senior Software Engineer",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `Versatile software engineer with 6+ years of experience in backend and frontend development. Passionate about building secure, scalable, and user-friendly systems. Skilled in managing multiple deadlines and leveraging the latest technologies for efficient full-stack development.`,
+    contact: {
+      email: "wengkeiting2@gmail.com",
+      // phone: "+91-9919716378",
+      // address: "Bangalore, IN",
+    },
+  },
+  links: [
+    {
+      title: "LinkedIn",
+      src: "https://www.linkedin.com/in/tingwengkei/",
+      iconClass: "fa-brands fa-linkedin-in",
+    },
+    // {
+    //   title: "Github",
+    //   src: "https://github.com/imvpn22",
+    //   iconClass: "fa-brands fa-github",
+    // },
+    // {
+    //   title: "Codepen",
+    //   src: "https://codepen.io/imvpn22",
+    //   iconClass: "fa-brands fa-codepen",
+    // },
+  ],
+  skills: [
+    {
+      title: "Backend",
+      value: "PHP, Laravel",
+    },
+    {
+      title: "Frontend",
+      value: "HTML CSS JavaScripts, VueJS, Tailwind CSS, .net",
+    },
+    {
+      title: "Backend",
+      value: "MySQL",
+    },
+    {
+      title: "Others",
+      value: "Git",
+    },
+  ],
+  experiences: [
+    {
+      organization: "Set Gaji SDN BHD",
+      title: "Senior Backend Developer",
+      date: "DEC 2024 - PRESENT",
+      details: [
+        `Company Overview: Finance Platform (Early access your daily collected salary platform)`,
+        `Maintained the existing platform by troubleshooting bugs, optimizing code performance, implementing new features, and ensuring system updates and database optimizations were applied efficiently`,
+        `Designed and implemented scalable backend systems using <strong>PHP (Laravel)</strong> and <strong>Node.js</strong>, ensuring smooth integration with front-end components`,
+        `Refactored legacy code to improve maintainability and align with modern programming techniques`,
+        `Collaborated with cross-functional teams to design and implement new features, ensuring timely project completion`,
+        `Developed scalable APIs for seamless integration with front-end applications, improving overall user experience`,
+        `Collaborated with front-end developers to ensure seamless integration of back-end functionality into user interfaces`,
+
+      ],
+    },
+    {
+      organization: "Efiankey SDN BHD",
+      title: `Senior Software Engineer (Backend)`,
+      date: "June 2022 - Nov 2024",
+      details: [
+        `Developed and maintained the backend architecture for a multi-level marketing platform (MLM Platform), utilizing <strong>PHP</strong> and <strong>MySQL</strong> for scalable, secure, and high-performance solutions.`,
+        `Designed and implemented efficient bonus calculation algorithms for MLM platforms, ensuring accurate payouts and seamless integration with financial systems.`,
+        `Designed and implemented RESTful APIs for seamless communication between front-end and back-end systems.`,
+        `Integrated third-party APIs to enhance platform functionality, including payment gateways, user authentication, and reporting tools.`,
+        `Optimized backend services for performance and security, ensuring data integrity and handling high-traffic loads efficiently.`,
+        `Integrated <strong>Apple Sign-In</strong> using OAuth 2.0 to enable secure authentication via Apple ID.`,
+        `Integrated <strong>Google Login</strong> and <strong>Google Drive API</strong> with OAuth 2.0 to allow users to authenticate using their Google account and retrieve files from their Google Drive.`,
+        `Collaborated closely with cross-functional teams, including designers, product managers, and backend developers, to create seamless user experiences.`,
+        `Advised on best practices for <strong>HTML5</strong>, <strong>CSS3</strong>, and <strong>JavaScript</strong> for Frontend Team, tailoring recommendations to meet specific project needs.`,
+        `Ensured system uptime with <strong>24/7 live support</strong>, addressing technical issues and providing real-time troubleshooting for end-users.`,
+
+      ],
+    },
+    {
+      organization: "Jenexus Holding SDN BHD",
+      title: `Frontend Developer`,
+      date: "June 2022 - Nov 2024",
+      details: [
+        `Developed and maintained the user interface (UI) for a CRM system, focusing on intuitive designs and seamless user experience using <strong>HTML</strong>, <strong>CSS</strong>, and <strong>Vue.js</strong>.`,
+        `Created responsive, mobile-first UI designs for rental and parking platforms, ensuring smooth interactions on both web and mobile interfaces.`,
+        `Designed mobile applications using <strong>Flutter</strong> and <strong>uni-app</strong>, building cross-platform experiences that enhanced user engagement and accessibility.`,
+        `Delivered front-end solutions with a strong focus on performance, usability, and clean design principles, ensuring consistency across different devices and screen sizes.`,
+        `Slightly involve backend architecture for various platforms using <strong>Python</strong>, <strong>FeatherJS</strong>, and <strong>Golang</strong>, ensuring a highly scalable and maintainable system.`,
+        `Slightly involve <strong>python</strong> to implement a Telegram bot to handle automated chatting and customer interactions, enhancing user engagement and support.`,
+
+      ],
+    },
+    {
+      organization: "Red Dragon",
+      title: `Internship Programmer (Frontend)`,
+      desc: `Internship Programmer (Frontend)`,
+      date: "June 2022 - Nov 2024",
+      details: [
+        `Developed and maintained the HR Portal with a focus on both front-end and back-end systems.`,
+        `Designed and implemented the frontend using <strong>ASP.NET</strong>, ensuring a responsive and user-friendly interface that supported HR management features such as leave requests, payroll, and employee records.`,
+        `Built the backend with C and Visual Basic, ensuring efficient data processing and integration with various internal systems.`,
+        `Led the initiative to improve the database structure, optimizing data relationships and performance for faster query processing.`,
+        `Migrated the HR portal's database from MsSQL to MySQL, ensuring minimal downtime and smooth transition of data, which resulted in improved database performance and reduced operational costs.`,
+        `Collaborated with designers to amend UI elements based on feedback, ensuring a polished and consistent user interface across different HR features.`,
+        `Developed the UI of a charity website from scratch, translating design mockups into fully functional web pages.`,
+      ],
+    },
+  ],
+  // projects: [
+  //   {
+  //     title: "Restaurant Review",
+  //     duration: "Jun - Nov 2018",
+  //     link: "https://github.com/imvpn22/restaurant-review-pwa",
+  //     desc: `A web app to list, view and add reviews of restaurants.
+  //     The app includes filtering based on location, cuisine, and rating.
+  //     This project was a part of Udacity's <strong>Mobile Web Specialist</strong> nano degree program.
+  //     Developed using <strong>HTML5</strong>, <strong>CSS3</strong> and <strong>JavaScript</strong>
+  //     with Progressive Web-Application standards including full-responsiveness,
+  //     offline support, and improved accessibility.`,
+  //   },
+  //   {
+  //     title: "WhiteBoard",
+  //     duration: "Jun - Aug 2017",
+  //     link: "https://github.com/imvpn22/whiteboard",
+  //     desc: `A simple <strong>web-app for collaborative brainstorming sessions</strong>.
+  //     The app includes a real-time messaging and sketching platform for collaboration with Peers.
+  //     It also has features to create groups(teams) and add/remove members.
+  //     Developed using <strong>Node.js-Express</strong> with server-side-templating (EJS),
+  //     <strong>CSS</strong>, <strong>JavaScript</strong> and <strong>Socket.io</strong>.`,
+  //   },
+  // ],
+  education: [
+    {
+      alma: "Help University",
+      duration: "May 2017 - July 2019",
+      std: "Diploma in Information Technology",
+      // score: "3.25",
+    },
+  ],
+  // certifications: [
+  //   {
+  //     desc: `<strong>Mobile Web Specialist Nanodegree</strong> by <strong>Udacity</strong>.
+  //     (<a target='_blank' rel='noreferrer' href='https://confirm.udacity.com/RLMHXAWZ'>https://goo.gl/RGRzVn</a>)`,
+  //     date: "May - Nov 2018",
+  //   },
+  //   {
+  //     desc: `<strong>Introduction to Modern Application Development</strong>, a MOOC by <em> NPTEL (IIT Madras)
+  //     and Hasura</em>. Ranked at <strong> top 10 percentile </strong> among more than 2500 candidates.
+  //      (<a target='_blank' rel='noreferrer' href='https://nptel.ac.in/noc/E_Certificate/linkedin/noc17-cs06/NPTEL17CS0626270067AN.jpg'>
+  //      https://goo.gl/X3HEdR</a>)`,
+  //     date: "Jan - Mar 2017",
+  //   },
+  // ],
+  events: [],
+};
